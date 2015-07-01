772ba7f39b362258bf53b0965dd6e17c55626ee4
Mustc <tarena11@dayenidejiangchoule.local>
2015-07-01 11:14:56 +0800

    ss

--- sdufh/sdufh/ViewController.m
+++ sdufh/sdufh/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"kjfhgkdhg");
+    NSLog(@"kjfhgkdhgkshfs");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
